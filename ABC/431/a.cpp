23c1cd0cb9701a1b41e7121f2d18f79b0d49cb8f
kanazawa <dogadogasan@gmail.com>
2025-11-17 21:56:16 +0900

    fix: 実行ファイルが自動削除されるように変更

--- ABC/431/a.cpp
+++ ABC/431/a.cpp
@@ -3,12 +3,16 @@ using namespace std;
 #define rep(i,n) for(int i=0;i<(n);i++)
 using ll = long long;
 
+/*
+入力の方法から思い出さないといけない。
+*/
+
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   // ---- solve ----
   ll h,b;
-  cin >> h >> w;
-  
+  cin >> h >> b;
+  cout << h << b << endl;
   return 0;
 }
