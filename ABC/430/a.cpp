51396526815d0755980634ec29a8b790eed9ade7
kanazawa <dogadogasan@gmail.com>
2025-11-19 00:08:17 +0900

    20251119

---
+++ ABC/430/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(n);i++)
+using ll = long long;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  // ---- solve ----
+  ll a,b,c,d;
+  cin >> a >> b >> c >> d;
+  if (c>=a && d<b) cout << "Yes" << endl;
+  else cout << "No" << endl;
+
+  return 0;
+}
